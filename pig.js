7d4676291681be39ae83de73ce5921b02e67164d
Dany Cervantes <danycervantesq@gmail.com>
2022-10-30 16:21:09 -0400

    Update pig.js
    Figured out how to make it work with a sentence, using an array to get to work on one word at a time.
    Still need to fix how to display capitalization, and special characters.

--- pig.js
+++ pig.js
@@ -4,34 +4,36 @@ let pigLatinText = document.querySelector(".pig-latin-text");
 
 function pigLatin(str) {
     
-  let word = str;
-  let oneVowel = /\s?([aeiou])\s?/gi //contains only one vowel
-  let vowel = /\s?([aeiou]\w+)\s?/gi; //starts with a vowel, followed by other characters
-  let novowel = /\s?([^aeiou][^aeiou]+)\s?/gi; //doesn't start with a vowel, or have any vowels in the word
-  let consonant = /\s?([^aeiou]+)(\w+)\s?/gi; //doesn't start with a vowel, followed by other characters
-  
+  let words = str.split(" "); //turns user sentence into array separated at blank spaces
 
-  if(oneVowel.test(str)) {
+  let oneVowel = /^([aeiou])$/gi; //contains only one vowel
+  let vowel = /^([aeiou]\w+)$/gi; //starts with a vowel, followed by other characters
+  let novowel = /^([^aeiou][^aeiou]+)$/gi; //doesn't start with a vowel, or have any vowels in the word
+  let consonant = /^([^aeiou]+)(\w+)$/gi; //doesn't start with a vowel, followed by other characters
 
-    word = str[0].toUpperCase()+str.slice(1); //makes new 1st letter uppercase, adds rest of string
-    word = word.replace(oneVowel, " $1way "); //if word contains only one vowel
-  }
-  if(vowel.test(str)) {
 
-    word = str[0].toUpperCase()+str.slice(1); //makes new 1st letter uppercase, adds rest of string
-    word = word.replace(vowel, " $1way "); //if word starts /w a vowel, adds "way" at the end
-  } 
-  if(novowel.test(str)) {
-    word = str.replace(novowel, " $1ay "); //if word doesn't start or contain any vowels, adds "ay" at end
+  for(let i = 0; i < words.length; i++) {
+
+    if(oneVowel.test(words[i])) {
+      words[i] = words[i].replace(oneVowel, "$1way"); //if word contains only one vowel
+    } else if(vowel.test(words[i])) {
+      words[i] = words[i].replace(vowel, "$1way"); //if word starts /w a vowel, adds "way" at the end
+    } 
+    if(novowel.test(words[i])) {
+      words[i] = words[i].replace(novowel, "$1ay"); //if word doesn't start or contain any vowels, adds "ay" at end
+    } else if (consonant.test(words[i])) { //if word starts with a consonant, and contains vowels
+      words[i] = words[i].replace(consonant, "$2$1ay"); //sends 1st consonant to the end, and adds "ay" to the end
+    }
   }
-  if (consonant.test(str)) { //if word starts with a consonant, and contains vowels
-    word = str[0].toLowerCase()+str.slice(1); //makes 1st consonant lowercase, adds rest of string
-    word = word.replace(consonant, " $2$1ay "); //sends 1st consonant to the end, and adds "ay" to the end
-    word = word[1].toUpperCase()+word.slice(2); //makes new 1st letter uppercase, adds rest of string
-  }
+
+  words = words.join(" ");
+
+/*
+  word = str[0].toLowerCase()+str.slice(1); //makes 1st consonant lowercase, adds rest of string
+  word = word[1].toUpperCase()+word.slice(2); //makes new 1st letter uppercase, adds rest of string
+*/
     
-
-  pigLatinText.innerHTML = word; 
+  pigLatinText.innerHTML = words; 
 
   if(pigLatinText.innerHTML == "") {
   //if user deletes all characters, initial message returns
